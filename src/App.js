da6bb240daaaf8153a2382aa4c650aae45d3c914
alhbib699 <alhbib699@gmail.com>
2026-04-16 08:19:11 +0300

    add footer statistics component

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function handleDeleteItems(id){
       <Logo />
       <Form onAddItem={handleAddItems}/>
       <PackingList items={items } onDeleteItem={handleDeleteItems} onToggleItem={handleToggleItem}/>
-      <Stats />
+      <Stats  items ={items}/>
     </div>
   );
 }
@@ -83,11 +83,17 @@ function Item({ item , onDeleteItem, onToggleItem}) {
   </li>
 }
 
-function Stats() {
+function Stats({items}) {
+  if(!items.length) return <p className="stats"><em>Start adding items to your packing list!🚀</em></p>
+  const numberOfItems = items.length;
+  const numberOfPacked = items.filter(item => item.packed).length;
+  const percentPacked = numberOfItems === 0 ? 0 : Math.round((numberOfPacked / numberOfItems) * 100);
   return <footer className="stats">
     {/* <em>🧳You have X item on your list, and you already packed X (X%)</em> */}
-    <em>You packed packedItems out of totalItems items
-(percent ready for travel ✈️)</em>
+    <em>
+      {percentPacked === 100 ? "You are ready to go! 🧳✈️" :
+      `You packed ${numberOfPacked} out of ${numberOfItems} items
+(${percentPacked}% ready for travel ✈️)`}</em>
   </footer>
 }
 
