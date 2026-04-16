d9c557464072b8894568492ac7551e0d71af5350
alhbib699 <alhbib699@gmail.com>
2026-04-16 11:29:58 +0300

    implement sorting and clear list functionality

--- src/App.js
+++ src/App.js
@@ -16,12 +16,15 @@ function handleToggleItem(id){
 function handleDeleteItems(id){
   setItems(items => items.filter(item => item.id !== id));
 }
+function handleClearList(){
+  setItems([]);
+}
 
   return (
     <div className="app">
       <Logo />
       <Form onAddItem={handleAddItems}/>
-      <PackingList items={items } onDeleteItem={handleDeleteItems} onToggleItem={handleToggleItem}/>
+      <PackingList items={items } onClearList={handleClearList} onDeleteItem={handleDeleteItems} onToggleItem={handleToggleItem}/>
       <Stats  items ={items}/>
     </div>
   );
@@ -64,13 +67,28 @@ const [quantity, setQuantity] = useState(1);
 }
 
 
-function PackingList({items , onDeleteItem , onToggleItem}) {
+function PackingList({items , onDeleteItem , onToggleItem , onClearList}) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems ;
+  if(sortBy === "input") sortedItems = items;
+  if( sortBy === "description") sortedItems = items.slice().sort((a,b) => a.description.localeCompare(b.description));
+  if( sortBy === "packed") sortedItems = items.slice().sort((a,b) => Number(a.packed) - Number(b.packed));
+
   return(
     <div className="list">
-    <ul>
-      {items.map((item) => (<Item item={item} key={item.id} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem}
+      <ul>
+      {sortedItems.map((item) => (<Item item={item} key={item.id} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem}
       />))}
-    </ul>
+      </ul>
+      <div className="actions">
+      <select value={sortBy} onChange={(e)=> setSortBy(e.target.value)}>
+        <option value="input">Sort by input order</option>
+        <option value="description">Sort by description</option>
+        <option value="packed">Sort by packed status</option>
+      </select>
+      <button onClick={()=> onClearList()}>Clear list</button>
+      </div>
     </div>
   );
 }
